6726a76821b913c819e1e861ab568cd27a3a802a
piotrwisniewskii <piowisni@gmail.com>
2026-02-14 09:45:14 +0100

    Added comment to test git

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,6 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
+        // My first change to be commited.
         [Key]
         public int CartItemId { get; set; }
 
